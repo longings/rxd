a5ebd5b32d1f713b4b03f93e3a25e7cbb654acb4
Jack <geolyhandian@gmail.com>
2017-07-04 21:35:05 +0800

    Nginx server the Statics when the settings.DEBUG is False

--- rxd/rxd/urls.py
+++ rxd/rxd/urls.py
@@ -1,5 +1,5 @@
 """
--------------Freedom!!!!!!!!--------------------
+-------------RXD--------------------
 """
 from django.conf import settings
 from django.conf.urls import url, include
@@ -8,11 +8,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
